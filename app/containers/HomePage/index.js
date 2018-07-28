5ea750c18027e936e6857bab9b07ea2e21eecabf
Steven Payne <steven.g.payne@hotmail.com>
2018-07-28 12:03:37 +1000

    Partial frame generation

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,17 +9,22 @@
  * the linting exception.
  */
 
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import messages from './messages'
+
+import VideoSplitter from '../../components/VideoSplitter'
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
-  render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>
+                    <FormattedMessage {...messages.header} />
+                </h1>
+                <VideoSplitter />
+            </div>
+        )
+    }
 }
